a908fc63057081d4543fdc9fec8a4d4e543f2048
DON ELCID P. ESPELETA <2202610@cc.usls.edu>
2025-01-30 11:16:13 +0800

    Finish routing

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+    <title>Document</title>
+</head>
+<body>
+    <h1>Login</h1>
+
+    <div class ="container">
+    <div class ="row">
+        <div class="col-lg-12">
+            <div class="col-lg-6">
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                </div>
+                <div class="mb-3">
+                <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            </div>
+                
+        </div>
+        </div>
+    </div>
+    </div>
+</body>
+</html>
